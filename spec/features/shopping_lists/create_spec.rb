9a9f552d4ac67fb958ca7c1f876b412942b33186
Jørgen Sundt <jorgen@theeleven.co.uk>
2015-03-27 20:21:40 +0000

    Refactor todo list creation in spec

--- spec/features/shopping_lists/create_spec.rb
+++ spec/features/shopping_lists/create_spec.rb
@@ -1,14 +1,22 @@
 require 'spec_helper'
 
 describe "Creating lists" do
-  it "redirects to the index page on success" do
+  def create_lists(options={})
+    options[:title] ||= "My todo list"
+    options[:description] ||= "This is my todo list."
+
     visit "/shopping_lists"
     click_link "New Shopping list"
     expect(page).to have_content("New Shopping List")
 
-    fill_in "Title", with: "My todo list"
-    fill_in "Description", with: "This is what I need"
+    fill_in "Title", with: options[:title]
+    fill_in "Description", with: options[:description]
     click_button "Create Shopping list"
+  end
+
+
+  it "redirects to the index page on success" do
+    create_lists
 
     expect(page).to have_content("My todo list")
   end
@@ -16,13 +24,7 @@ describe "Creating lists" do
   it "displays an error when you forget the title" do
     expect(ShoppingList.count).to eq(0)
 
-    visit "/shopping_lists"
-    click_link "New Shopping list"
-    expect(page).to have_content("New Shopping List")
-
-    fill_in "Title", with: ""
-    fill_in "Description", with: "This is what I need"
-    click_button "Create Shopping list"
+    create_lists title: ""
 
     expect(page).to have_content("error")
     expect(ShoppingList.count).to eq(0)
@@ -34,13 +36,7 @@ describe "Creating lists" do
   it "displays an error when the title is less than 3 characters" do
     expect(ShoppingList.count).to eq(0)
 
-    visit "/shopping_lists"
-    click_link "New Shopping list"
-    expect(page).to have_content("New Shopping List")
-
-    fill_in "Title", with: "Hi"
-    fill_in "Description", with: "This is what I need"
-    click_button "Create Shopping list"
+    create_lists title: "Hi"
 
     expect(page).to have_content("error")
     expect(ShoppingList.count).to eq(0)
@@ -52,13 +48,7 @@ describe "Creating lists" do
   it "displays an error when the list has no Description" do
     expect(ShoppingList.count).to eq(0)
 
-    visit "/shopping_lists"
-    click_link "New Shopping list"
-    expect(page).to have_content("New Shopping List")
-
-    fill_in "Title", with: "List number 1"
-    fill_in "Description", with: ""
-    click_button "Create Shopping list"
+    create_lists description: ""
 
     expect(page).to have_content("error")
     expect(ShoppingList.count).to eq(0)
@@ -70,13 +60,7 @@ describe "Creating lists" do
   it "displays an error when the Description has less than 5 characters" do
     expect(ShoppingList.count).to eq(0)
 
-    visit "/shopping_lists"
-    click_link "New Shopping list"
-    expect(page).to have_content("New Shopping List")
-
-    fill_in "Title", with: "List number 1"
-    fill_in "Description", with: "Food"
-    click_button "Create Shopping list"
+    create_lists description: "Food"
 
     expect(page).to have_content("error")
     expect(ShoppingList.count).to eq(0)
