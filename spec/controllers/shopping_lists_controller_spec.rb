f355136af8e17d36c60e23321f0193f467b7e7b6
Jørgen Sundt <jorgen@theeleven.co.uk>
2015-03-27 20:55:06 +0000

    Add specs for deleting todos

--- spec/controllers/shopping_lists_controller_spec.rb
+++ spec/controllers/shopping_lists_controller_spec.rb
@@ -23,7 +23,7 @@ describe ShoppingListsController do
   # This should return the minimal set of attributes required to create a valid
   # ShoppingList. As you add validations to ShoppingList, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "title" => "MyString" } }
+  let(:valid_attributes) { { "title" => "MyString", "description" => "My Description" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
