a0433f97717fe86136a6b65130d4c3b181eb3e09
Jørgen Sundt <jorgen@theeleven.co.uk>
2015-03-28 01:00:46 +0000

    Edit items

--- app/controllers/shopping_items_controller.rb
+++ app/controllers/shopping_items_controller.rb
@@ -21,6 +21,28 @@ class ShoppingItemsController < ApplicationController
     end
   end
 
+  def edit 
+    @shopping_list = ShoppingList.find(params[:shopping_list_id])
+    @shopping_item = @shopping_list.shopping_items.find(params[:id])
+  end
+
+  def update 
+    @shopping_list = ShoppingList.find(params[:shopping_list_id])
+    @shopping_item = @shopping_list.shopping_items.find(params[:id])
+
+    if @shopping_item.update_attributes(shopping_item_params)
+      flash[:success] = "Updated List Item."
+      redirect_to shopping_list_shopping_items_path
+    else
+      flash[:error] = "There was a problem saving the item"
+      render action: :edit
+    end
+  end
+
+  def url_options
+    { shopping_list_id: params[:shopping_list_id] }.merge(super)
+  end
+
   private
   def shopping_item_params
     params[:shopping_item].permit(:content)
---
+++ spec/features/shopping_items/edit_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe "Editing todo items" do
+  let!(:todo_list) { ShoppingList.create(title: "Groceries", description: "Grocery List.") }
+  let!(:todo_item) { todo_list.shopping_items.create(content: "Milk") }
+
+  def visit_list(list)
+    visit "/shopping_lists"
+    within "#shopping_list_#{list.id}" do
+      click_link "List Items"
+    end
+  end
+
+  it "is successful with valid content" do
+    visit_list(todo_list)
+    within("#shopping_item_#{todo_item.id}") do
+      click_link "Edit"
+    end
+
+    fill_in "Content", with: "Lots of Milk"
+    click_button "Save"
+
+    expect(page).to have_content("Updated List Item.")
+    todo_item.reload
+    expect(todo_item.content).to eq("Lots of Milk")
+  end
+
+  it "is unsuccessful with no content" do
+    visit_list(todo_list)
+    within("#shopping_item_#{todo_item.id}") do
+      click_link "Edit"
+    end
+
+    fill_in "Content", with: ""
+    click_button "Save"
+
+    expect(page).to_not have_content("Updated List Item.")
+    expect(page).to have_content("Content can't be blank")
+    todo_item.reload
+    expect(todo_item.content).to eq("Milk")
+  end
+
+  it "is unsuccessful with not enough content" do
+    visit_list(todo_list)
+    within("#shopping_item_#{todo_item.id}") do
+      click_link "Edit"
+    end
+
+    fill_in "Content", with: "1"
+    click_button "Save"
+
+    expect(page).to_not have_content("Updated List Item.")
+    expect(page).to have_content("Content is too short")
+    todo_item.reload
+    expect(todo_item.content).to eq("Milk")
+  end
+
+end
