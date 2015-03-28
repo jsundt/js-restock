70992ab8c31b3e7c5771e8080f1fe388b299882e
Jørgen Sundt <jorgen@theeleven.co.uk>
2015-03-28 01:16:03 +0000

    Delete Items

--- app/controllers/shopping_items_controller.rb
+++ app/controllers/shopping_items_controller.rb
@@ -1,15 +1,15 @@
 class ShoppingItemsController < ApplicationController
+
+  before_action :find_shopping_list
+
   def index
-    @shopping_list = ShoppingList.find(params[:shopping_list_id])
   end
 
   def new
-    @shopping_list = ShoppingList.find(params[:shopping_list_id])
     @shopping_item = @shopping_list.shopping_items.new
   end
 
   def create
-    @shopping_list = ShoppingList.find(params[:shopping_list_id])
     @shopping_item = @shopping_list.shopping_items.new(shopping_item_params)
 
     if @shopping_item.save
@@ -22,12 +22,10 @@ class ShoppingItemsController < ApplicationController
   end
 
   def edit 
-    @shopping_list = ShoppingList.find(params[:shopping_list_id])
     @shopping_item = @shopping_list.shopping_items.find(params[:id])
   end
 
   def update 
-    @shopping_list = ShoppingList.find(params[:shopping_list_id])
     @shopping_item = @shopping_list.shopping_items.find(params[:id])
 
     if @shopping_item.update_attributes(shopping_item_params)
@@ -39,11 +37,27 @@ class ShoppingItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @shopping_item = @shopping_list.shopping_items.find(params[:id])
+    if @shopping_item.destroy
+      flash[:success] = "List item was deleted."
+    else
+      flash[:error] = "Item could not be deleted."
+    end
+    redirect_to shopping_list_shopping_items_path
+  end
+
+
   def url_options
     { shopping_list_id: params[:shopping_list_id] }.merge(super)
   end
 
+
   private
+  def find_shopping_list
+    @shopping_list = ShoppingList.find(params[:shopping_list_id])
+  end
+
   def shopping_item_params
     params[:shopping_item].permit(:content)
   end
